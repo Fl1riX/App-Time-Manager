b578f02b8b9e9a86b9e5ff9ea51098d19826051a
Squ1reX <maksimhripinkov658@gmail.com>
2025-02-10 20:37:38 +0300

    working

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import psutil, sqlite3, logging, sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QWidget, QVBoxLayout, QPushButton, QDesktopWidget, QLabel
 from datetime import datetime
 
-# Настройка логирования
 logging.basicConfig(
     level=logging.INFO,
     filename="manager_logs.log",
@@ -13,10 +11,28 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-def find_proc(name):
+def get_inf_from_db():
     try:
+        with sqlite3.connect("data.db") as con:
+            cur = con.cursor()
+
+            cur.execute("CREATE TABLE IF NOT EXISTS data (name TEXT, time TEXT, date TEXT)")
+            con.commit()
+
+            cur.execute("SELECT name, MAX(time) FROM data GROUP BY name")
+            result = cur.fetchall()  
+            logging.info(f"✅ Результаты из базы данных: {result}")
+
+        return result
+    except Exception as e:
+        logging.error(f"❌ Ошибка в болучении данных из бд: {e}")
+        return 0
+
+def main(name):
+    try:
+        get_inf_from_db()
+        
         min_time_list = []
-        pid_list = []
 
         logging.info(f"🔍 Поиск процесса: {name}")
 
@@ -25,12 +41,10 @@ def find_proc(name):
             if p.info['name'] == name:
                 # Извлечение времени старта процесса
                 start_time_str = str(p).split("started=")[1].split("'")[1]
-                pid_str = str(p).split("pid=")[1].split(",")[0]
 
                 # Преобразование времени старта в минуты
                 start_time = int(start_time_str.split(":")[0]) * 60 + int(start_time_str.split(":")[1])
 
-                pid_list.append(pid_str)
                 min_time_list.append(start_time)
 
         if not min_time_list:
@@ -46,20 +60,16 @@ def find_proc(name):
 
         # Подсчет времени работы приложения
         min_app_time = now_time - start_time
-        pid = pid_list[min_time_list.index(start_time)]
-        app_path = psutil.Process(int(pid)).exe()
 
         try:
             with sqlite3.connect("data.db") as con:
                 cur = con.cursor()
-                cur.execute("CREATE TABLE IF NOT EXISTS data (name TEXT, time TEXT, path TEXT)")
-                con.commit()
 
                 time_str = f"{min_app_time} минут" if min_app_time < 60 else f"{round(min_app_time / 60, 2)} часа"
-                cur.execute("INSERT INTO data (name, time, path) VALUES (?, ?, ?)", (name, time_str, app_path))
+                cur.execute("INSERT INTO data (name, time, date) VALUES (?, ?, ?)", (name, time_str, datetime.now().replace(second=0, microsecond=0)))
                 con.commit()
 
-                logging.info(f"💾 Данные записаны: {name}, {time_str}, {app_path}")
+                logging.info(f"💾 Данные записаны: {name}, {time_str}")
 
         except sqlite3.Error as e:
             logging.error(f"❌ Ошибка SQLite: {e}")
@@ -76,23 +86,81 @@ def find_proc(name):
     except Exception as e:
         logging.error(f"❌ Неизвестная ошибка: {e}")
 
+class InfoWindow(QWidget):
+    def __init__(self, title):
+        logging.info(f"Открыта информация о {title}")
+        super().__init__()
+        
+        self.setWindowTitle(title)
+        self.resize(400, 300)
+        
+        self.setStyleSheet("background-color: black;")
+        
+        layout = QVBoxLayout()
+        
+        self.label = QLabel("тут текст")
+        self.label.setStyleSheet("QLabel { color: white; font-size: 15px}")
+        
+        layout.addWidget(self.label)
+        
+        self.setLayout(layout)
+        
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        
         self.setWindowTitle("App Time Manager")
-        self.setGeometry(500, 150, 800, 500)
+        self.resize(800, 500)
+        
+        self.setStyleSheet("background-color: black;")
+        
+        central_widget = QWidget()
+        
+        layout = QVBoxLayout()
+        
+        self.text_list = QListWidget()
+        self.text_list.itemClicked.connect(self.on_item_clicked)
+        
+        if get_inf_from_db() != 0:
+            for i in get_inf_from_db():
+                self.text_list.addItems([str(i).split(",")[0].split("'")[1]])
+        else:
+            sys.exit()
+            logging.error("❌ Ошибка при получении информации из бд, закрытие приложения.")
+            
+        self.text_list.setStyleSheet("QListWidget { color: white; font-size: 15px}")
+        self.setCentralWidget(central_widget)
+        
+        self.add_btn = QPushButton("Add")
+        self.add_btn.setStyleSheet("QPushButton { color: white; font-size: 15px}")
+        self.add_btn.clicked.connect(lambda:print("Button pressed!"))
+        
+        layout.addWidget(self.text_list)
+        layout.addWidget(self.add_btn)
+        central_widget.setLayout(layout)
+        
+    def on_item_clicked(self, item):
+        self.inf_win = InfoWindow(item.text())
+        self.inf_win.show()
 
 class AppTimeManager:   
     def __init__(self):
         self.app = QApplication(sys.argv)
         self.main_win = MainWindow()
     
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+    
     def run(self):
         self.main_win.show()
         sys.exit(self.app.exec_())
+        
 
 if __name__ == "__main__":
-    find_proc("Code.exe")
+    main("Code.exe")
     
     app_manager = AppTimeManager()
     app_manager.run()
